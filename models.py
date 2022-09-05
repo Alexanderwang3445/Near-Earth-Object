c93a803a4f445ebd1cf593ea4f81746c02a90809
Alexanderwang3445 <112518664+Alexanderwang3445@users.noreply.github.com>
2022-09-05 17:11:28 -0400

    Update and rename main.py to models.py

--- models.py
+++ models.py
@@ -34,7 +34,7 @@ class NearEarthObject:
     """
     # TODO: How can you, and should you, change the arguments to this constructor?
     # If you make changes, be sure to update the comments in this file.
-    def __init__(self, **info):
+    def __init__(self, designation, name, diameter, hazardous):
         """Create a new `NearEarthObject`.
 
         :param info: A dictionary of excess keyword arguments supplied to the constructor.
@@ -44,17 +44,15 @@ class NearEarthObject:
         # You should coerce these values to their appropriate data type and
         # handle any edge cases, such as a empty name being represented by `None`
         # and a missing diameter being represented by `float('nan')`.
-        self.designation = info.get('designation')
-        self.diameter = info.get('diameter')
-        self.hazardous = info.get('hazardous')
-        self.name = info.get('name')
-        if info.get('name')!='':
-            self.name = None
-        if info.get('hazardous')!='':
-            self.hazardous = False
-        if info.get('diameter')!='':
+        self.designation = designation
+        self.diameter = float(diameter)
+        self.hazardous = (hazardous=='Y')
+        self.name = name
+        if diameter == '':
             self.diameter = float('nan')
-        if info.get('designation')!='':
+        if name == '':
+            self.name=None
+        if designation == '':
             self.designation = None
         # Create an empty initial collection of linked approaches.
         self.approaches = []
@@ -93,7 +91,7 @@ class CloseApproach:
     """
     # TODO: How can you, and should you, change the arguments to this constructor?
     # If you make changes, be sure to update the comments in this file.
-    def __init__(self, **info):
+    def __init__(self, designation, time, distance, velocity):
         """Create a new `CloseApproach`.
 
         :param info: A dictionary of excess keyword arguments supplied to the constructor.
@@ -102,23 +100,17 @@ class CloseApproach:
         # onto attributes named `_designation`, `time`, `distance`, and `velocity`.
         # You should coerce these values to their appropriate data type and handle any edge cases.
         # The `cd_to_datetime` function will be useful.
-        self._designation = ''
-        if(self.designation!=''):
-            self.designation=info.get('designation')
-        self.time = None
-        if(self.time!=''):
-            self.time=cd_to_datetime(info.get('time'))
+        self.designation=designation
+        self.time=cd_to_datetime(time)
         # TODO: Use the cd_to_datetime function for this attribute.
         self.distance = 0.0
-        if(self.distance!=''):
-            self.distance=info.get('distance')
+        if(distance!=''):
+            self.distance=float(distance)
         self.velocity = 0.0
-        if(self.velocity!=''):
-            self.velocity=info.get('velocity')
+        if(velocity!=''):
+            self.velocity=float(velocity)
         # Create an attribute for the referenced NEO, originally None.
         self.neo = None
-        if(self.neo!=''):
-            self.neo=info.get('neo')
 
     @property
     def time_str(self):
